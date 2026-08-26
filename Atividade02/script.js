053257cebb5efd32f937003b5609e64a477712cf
DAVI AZEVEDO SCATOLON SILVA <55457261875@info.local>
2026-08-26 17:32:22 -0300

    01

---
+++ Atividade02/script.js
@@ -0,0 +1,85 @@
+//let idade = (prompt("digite sua idade"))
+//if(!(idade>=18))
+ //   console.log("Menor de idade")
+//else{
+ //   console.log("maior de idade")
+//}
+
+//let numero=(prompt("Um numero"))
+//if(!(numero<=0))
+ //   console.log("positivo")
+//else{
+   // console.log("negativo")
+//}
+
+//let nota=(prompt("nota do aluno"))
+//if (nota>=60) {
+   // console.log("Aprovado")
+//} else {
+  //  console.log("Reprovado")
+//}
+
+//let numero=(Number(prompt("um numero")))
+//if(numero>0)
+//    console.log("positivo")
+//else if(numero==0)
+  //  console.log("zero")
+//else if(numero<0)
+   // console.log("negativo")
+
+//let idade=(Number(prompt("Coloque sua idade")))
+//if(idade>=0&&idade<=12)
+ //   console.log("Criança")
+//else if(idade>=13&&idade<=17)
+//    console.log("Adolescente")
+//else if(idade>=18)
+ //   console.log("Adulto")
+
+//let numero=(Number(prompt("Um numero")))
+//let resultado = numero % 2
+//if(resultado === 0 ) 
+//{
+//    alert("par")
+//}else{
+ //   alert("impar")
+//}
+
+//const usuario="Davi"
+//const senha="1234"
+//let Usuario=(prompt("Usuario"))
+//let Senha=(prompt("Senha"))
+//if(!(Usuario==usuario))
+    //alert("Usuario incorreto")
+//else{
+    //console.log("Entrada permitida")
+//}
+//if(!(Senha==senha))
+   // alert("Senha Incorreta")
+//else{
+   // console.log("Entrada permitida")
+//}
+
+//const numero01=Number(prompt("digite um numero"))
+//const operaçao=prompt("digite uma operaçao")
+//const numero02=Number(prompt("digite um numero"))
+//let resultado
+//if (operaçao === "+") {
+  ///  resultado=numero01+numero02
+ //   console.log(resultado)
+//}
+//if (operaçao === "-") {
+// resultado=numero01-numero02
+    //console.log(resultado)
+//}
+//if (operaçao === "*") {
+//resultado=numero01*numero02
+   // console.log(resultado)
+//}
+//if (operaçao === "/") {
+  //  if(numero02==0)
+   //     console.log("erro")
+//resultado=numero01/numero02
+ //   console.log(resultado)
+//}
+    
+
