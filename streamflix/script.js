f8e56bab3f50c59b7876e7a877f74eb45348e89a
DAVI AZEVEDO SCATOLON SILVA <55457261875@info.local>
2026-09-03 17:48:05 -0300

    🔥

---
+++ streamflix/script.js
@@ -0,0 +1,16 @@
+//DOCUMENT = PAGINA INTEIRA DO JS
+//getElementById -> procura pelo atributo id=""
+//titulo-filme -> id do filme
+const elemento = document.getElementById('filme-principal')
+console.log(elemento)
+console.log(elemento.tagName)
+
+function selecionarPorId(){
+const tituloFilme = document.getElementById('titulo-filme')
+
+console.log(tituloFilme)
+console.log(tituloFilme.tagName)
+console.log(tituloFilme.textContent)
+console.log(tituloFilme.id)
+tituloFilme.classList.add('elemento-selecionado')//adicionei um atributo.git
+}
