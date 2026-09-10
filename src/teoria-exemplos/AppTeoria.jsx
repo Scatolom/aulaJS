d829804e15d274d876ee43e0a5313655e962ee07
DAVI AZEVEDO SCATOLON SILVA <55457261875@info.local>
2026-09-10 17:53:57 -0300

    🙏

---
+++ src/teoria-exemplos/AppTeoria.jsx
@@ -0,0 +1,12 @@
+import Contador from"./exemplos/Cotador.jsx";
+import Saudacao from"./exemplos/saudação.jsx";
+import "./teoria.css"
+function AppTeoria(){
+    return(
+        <div>
+            <Saudacao nome="Lucas"/>
+            <Contador/>
+        </div>
+    )
+}
+export default AppTeoria;
---
+++ src/teoria-exemplos/exemplos/saudação.jsx
@@ -0,0 +1,5 @@
+function saudacao({nome}){
+    return <h1>Olá {nome}</h1>
+}
+
+export default saudacao;
